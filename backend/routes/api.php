c8dcca5e295d1ea30462a69c1e4d2130b554c2d1
Douglas-Albuquerque <douglasalbuquerq@gmail.com>
2025-10-17 17:10:03 -0300

    adição da tela mesas

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,5 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComandaController;
 
 Route::get('/mesas-disponiveis', [ComandaController::class, 'mesasDisponiveis']);
+Route::get('/mesas', [ComandaController::class, 'todasMesas']);
 Route::post('/pedido-cozinha', [ComandaController::class, 'pedidoCozinha']);
 Route::apiResource('comandas', ComandaController::class);
